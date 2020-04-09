95f4940d64ad029974724be02f393db785749dbb
mroetheli <32132580+mroetheli@users.noreply.github.com>
2020-04-09 14:14:43 +0200

    Update ui-slider.js

--- addon/components/ui-slider.js
+++ addon/components/ui-slider.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 const { keys, create } = Object; // jshint ignore:line
 const {computed, observer, $, A, run, on, typeOf, debug, defineProperty, get, set, inject, isEmpty} = Ember;  // jshint ignore:line
 const snake = thingy => {
